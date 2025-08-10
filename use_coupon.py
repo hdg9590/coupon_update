25baf46697775be70efd5b733b743f97ae7a1fba
hdg9590 <handa9590@gmail.com>
2025-08-11 02:39:12 +0900

    coupon_update

---
+++ use_coupon.py
@@ -0,0 +1,31 @@
+import pymysql
+import os
+
+def lambda_handler(event, context):
+    user_id = event['pathParameters']['id']
+
+    connection = pymysql.connect(
+        host=os.environ['DB_HOST'],
+        user=os.environ['DB_USER'],
+        password=os.environ['DB_PASSWORD'],
+        database=os.environ['DB_NAME'],
+        connect_timeout=5
+    )
+
+    try:
+        with connection.cursor() as cursor:
+            sql = "UPDATE users SET coupons = coupons - 1 WHERE id = %s AND coupons > 0"
+            cursor.execute(sql, (user_id,))
+        connection.commit()
+        return {
+            "statusCode": 200,
+            "body": "쿠폰 지급 완료"
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": str(e)
+        }
+    finally:
+        connection.close()
+
